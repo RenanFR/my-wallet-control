909cfabdb19a02e5467c133184bf3249b20ae18b
Renan Rodrigues <renanfr1047@gmail.com>
2020-04-16 22:18:17 -0300

    first commit

---
+++ src/main/java/com/wallet/control/mywalletcontrol/MyWalletControlApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.control.mywalletcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWalletControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWalletControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wallet/control/mywalletcontrol/MyWalletControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.control.mywalletcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWalletControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
